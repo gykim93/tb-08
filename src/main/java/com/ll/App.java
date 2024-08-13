23ee047437233acb742951700932310265ad09a4
gykim93 <gykim93@gmail.com>
2024-08-13 14:00:19 +0900

    1단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 
 class App {
     void run() {
-        System.out.println("프로그램 실행");
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령 ) ");
+        while (true) {
+            System.out.print("명령 ) ");
 
-        Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
 
-        String cmd = scanner.nextLine();
-        System.out.printf("입력하신 명령 : %s\n", cmd);
-
+            if (cmd.equals("종료")) {
+                break;
+            }
+        }
     }
 }
