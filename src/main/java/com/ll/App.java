38681b65360ad63362ecc8325c6823dd13ec2193
gykim93 <gykim1993@gmail.com>
2024-08-19 12:50:50 +0900

    getParamAsInt 함수 도입

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -65,6 +65,15 @@ class App {
     }
 
     void actionRemove(String cmd) {
+        int id = getParamAsInt(cmd, "id", 0);
+        if (id == 0){
+            System.out.println("id를 정확히 입력해주세요");
+            return;
+        }
+        System.out.printf("%d번 명언을 삭제합니다\n", id);
+    }
+
+    int getParamAsInt(String cmd, String paramName, int defaultValue) {
         String[] cmdBits = cmd.split("\\?", 2); //cmd에 삭제?id=1&archive=true
         String action = cmdBits[0]; // 삭제
         String queryString = cmdBits[1]; // id=1&archive=true
@@ -75,16 +84,22 @@ class App {
 
         int id = 0;
 
-        for (int i = 0; i < queryStringBits.length; i++){
+        for (int i = 0; i < queryStringBits.length; i++) {
             String queryParamStr = queryStringBits[i];
             String[] queryParamStrBits = queryParamStr.split("=", 2);
-            String paramName = queryParamStrBits[0]; // id
+            String _paramName = queryParamStrBits[0]; // id
             String paramValue = queryParamStrBits[1];// 1
 
-            if (paramName.equals("id")){
-                id = Integer.parseInt(paramValue); // 문자열을 정수로 변환
+            if (_paramName.equals(paramName)) {
+                try {
+                    //문제가 없을 경우
+                    return Integer.parseInt(paramValue);
+                } catch (NumberFormatException e) {
+                    // 문제가 생긴 경우
+                    return defaultValue;
+                }
             }
         }
-            System.out.printf("%d번 명언을 삭제합니다\n", id);
+        return defaultValue;
     }
 }
