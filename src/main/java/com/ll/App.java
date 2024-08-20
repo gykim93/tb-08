840b47b27493f0fd888681098adcefb74bff20d3
gykim93 <gykim1993@gmail.com>
2024-08-20 14:02:11 +0900

    수정완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -36,6 +36,9 @@ class App {
                 case "삭제":
                     actionRemove(rq);
                     break;
+                case "수정":
+                    actionModify(rq);
+                    break;
             }
         }
     }
@@ -52,7 +55,7 @@ class App {
         Quotation quotation = new Quotation(id, content, authorName);
         quotations.add(quotation);
 
-        System.out.printf("%d번 명언이 등록 되었습니다.\n",lastQuotationid);
+        System.out.printf("%d번 명언이 등록 되었습니다.\n", lastQuotationid);
     }
 
     void actionList() {
@@ -77,14 +80,15 @@ class App {
         }
         int index = getIndexOfQuotationById(id);
         if (index == -1) {
-            System.out.printf("%d번 명언은 존재하지 않습니다.\n",id);
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
             return;
         }
         quotations.remove(index);
 
         System.out.printf("%d번 명언을 삭제합니다\n", id);
     }
-    int getIndexOfQuotationById(int id){
+
+    int getIndexOfQuotationById(int id) {
         for (int i = 0; i < quotations.size(); i++) {
             Quotation quotation = quotations.get(i);
             if (quotation.id == id) {
@@ -92,5 +96,31 @@ class App {
             }
         }
         return -1;
+    }
+
+    void actionModify(Rq rq) {
+        int id = rq.getParamAsInt("id", 0);
+        if (id == 0) {
+            System.out.println("id를 정확히 입력해주세요.");
+            return;
+        }
+        int index = getIndexOfQuotationById(id);
+        if (index == -1) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+            return;
+        }
+        Quotation quotation = quotations.get(index);
+        System.out.printf("명언(기존) : %s\n", quotation.content);
+        System.out.println("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.printf("작가(기존) : %s\n", quotation.authorName);
+        System.out.println("작가 : ");
+        String authorName = scanner.nextLine();
+
+        quotation.content = content;
+        quotation.authorName = authorName;
+
+        System.out.printf("%d번 명언을 수정합니다.\n",id);
     }
 }
