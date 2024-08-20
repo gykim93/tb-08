4bfb187350403e4731d4a78469ef4d46357e0eff
gykim93 <gykim1993@gmail.com>
2024-08-20 12:26:46 +0900

    if문에서 switch문으로 교체, 기존 getParamAsint 함수 제거 Rq class 적용

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -23,19 +23,19 @@ class App {
 
             String cmd = scanner.nextLine();
             Rq rq = new Rq(cmd);
-            System.out.println("rq.getAction : " + rq.getAction());
-            System.out.println("rq.getParamAsInt : " + rq.getParamAsInt("id", 0));
 
-            if (cmd.equals("종료")) {
-                break;
-            } else if (cmd.equals("등록")) {
-                actionWrite();
-                System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationid);
-
-            } else if (cmd.equals("목록")) {
-                actionList();
-            } else if (cmd.startsWith("삭제?")) {
-                actionRemove(cmd);
+            switch (rq.getAction()) {
+                case "종료":
+                    return;
+                case "등록":
+                    actionWrite();
+                    break;
+                case "목록":
+                    actionList();
+                    break;
+                case "삭제":
+                    actionRemove(rq);
+                    break;
             }
         }
     }
@@ -67,42 +67,13 @@ class App {
         }
     }
 
-    void actionRemove(String cmd) {
-        int id = getParamAsInt(cmd, "id", 0);
-        if (id == 0){
+    void actionRemove(Rq rq) {
+        int id = rq.getParamAsInt("id", 0);
+        if (id == 0) {
             System.out.println("id를 정확히 입력해주세요");
             return;
         }
         System.out.printf("%d번 명언을 삭제합니다\n", id);
     }
 
-    int getParamAsInt(String cmd, String paramName, int defaultValue) {
-        String[] cmdBits = cmd.split("\\?", 2); //cmd에 삭제?id=1&archive=true
-        String action = cmdBits[0]; // 삭제
-        String queryString = cmdBits[1]; // id=1&archive=true
-
-        String[] queryStringBits = queryString.split("&");
-        // queryStringBtis[0] id=1
-        // queryStringBtis[1] archive=true
-
-        int id = 0;
-
-        for (int i = 0; i < queryStringBits.length; i++) {
-            String queryParamStr = queryStringBits[i];
-            String[] queryParamStrBits = queryParamStr.split("=", 2);
-            String _paramName = queryParamStrBits[0]; // id
-            String paramValue = queryParamStrBits[1];// 1
-
-            if (_paramName.equals(paramName)) {
-                try {
-                    //문제가 없을 경우
-                    return Integer.parseInt(paramValue);
-                } catch (NumberFormatException e) {
-                    // 문제가 생긴 경우
-                    return defaultValue;
-                }
-            }
-        }
-        return defaultValue;
-    }
 }
