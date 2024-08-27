c33370cb9703e6ce2de32fa6c1def9a1ef3a8513
gykim93 <gykim1993@gmail.com>
2024-08-27 10:29:15 +0900

    getter,setter 적용

--- src/main/java/com/ll/domain/App.java
+++ src/main/java/com/ll/domain/App.java
@@ -70,7 +70,7 @@ public class App {
 
         for (int i = quotations.size() - 1; i >= 0; i--) {
             Quotation quotation = quotations.get(i);
-            System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
+            System.out.printf("%d / %s / %s\n", quotation.getId(), quotation.getAuthorName(), quotation.getContent());
         }
     }
 
@@ -98,7 +98,7 @@ public class App {
         for (int i = 0; i < quotations.size(); i++) {
             Quotation quotation = quotations.get(i);
 
-            if (quotation.id == id) {
+            if (quotation.getId() == id) {
                 return i;
             }
         }
@@ -114,6 +114,26 @@ public class App {
             return; // 함수를 끝낸다.
         }
 
-        System.out.printf("%d번 명언을 수정합니다.\n", id);
+        int index = findQuotationIndexById(id);
+
+        if (index == -1) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+            return;
+        }
+
+        Quotation quotation = quotations.get(index);
+
+        System.out.printf("명언(기존) : %s\n", quotation.getContent());
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.printf("작가(기존) : %s\n", quotation.getAuthorName());
+        System.out.print("작가 : ");
+        String authorName = scanner.nextLine();
+
+        quotation.setContent(content);
+        quotation.setAuthorName(authorName);
+
+        System.out.printf("%d번 명언을 수정되었습니다.\n", id);
     }
 }
